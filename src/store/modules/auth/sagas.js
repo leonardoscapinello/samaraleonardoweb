ad7dc1c7e86c226bfd53dadf015c1c364581c737
Leonardo Scapinello <leonardo.scapinello@oppy.com.br>
2019-10-16 15:45:19 -0300

    setting authorization header on each request

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import { signInSuccess, signFailure } from './actions';
 
 import api from '~/services/api';
@@ -14,18 +15,29 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
-    /*
-    if (!user.provider) {
-      console.tron.error('Usuário não é um prestador');
-      return;
-    }
-    */
+    // if (!user.provider) {
+    //  toast.error('Você não tem permissão para acessar o sistema.');
+    //  return;
+    // }
+
+    api.defaults.headers.Authorization = `Baerer ${token}`;
 
     yield put(signInSuccess(token, user));
-    history.push(`/dashboard`);
+    toast.success(`Olá, ${user.name}. Seja bem-vindo(a)`);
+    history.push('/dashboard');
   } catch (err) {
+    toast.error('Usuário ou senha incorretos. Tente novamente.');
     yield put(signFailure());
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+function setToken({ payload }) {
+  if (!payload) return;
+  const { token } = payload.auth;
+  if (token) api.defaults.headers.Authorization = `Baerer ${token}`;
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
