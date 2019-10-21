9fce677402eb2bf31dc81216522f0f9ddf8d54e3
Leonardo Scapinello <leonardo.scapinello@oppy.com.br>
2019-10-21 09:39:45 -0300

    starting carousel cards

--- src/components/WalletsCard/index.js
+++ src/components/WalletsCard/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import OwlCarousel from 'react-owl-carousel';
 import { Container, Content } from './styles';
 
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 export default function WalletsCard() {
   return (
